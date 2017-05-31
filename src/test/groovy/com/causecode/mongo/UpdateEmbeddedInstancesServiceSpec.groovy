beaa1e4fb738aada0d9e65ab9a1bd63f9efc58f2
Nikhil Sharma <nikhil.sharma@causecode.com>
2017-05-31 12:16:23 +0530

    Added gradle-code-quality plugin.

--- src/test/groovy/com/causecode/mongo/UpdateEmbeddedInstancesServiceSpec.groovy
+++ src/test/groovy/com/causecode/mongo/UpdateEmbeddedInstancesServiceSpec.groovy
@@ -16,54 +16,57 @@ import test.EmTestDomainA
 import test.TestDomainA
 import test.TestDomainB
 
+/**
+ * Test cases for UpdateEmbeddedInstancesService class.
+ */
 @TestFor(UpdateEmbeddedInstancesService)
 @Mock([TestDomainA, TestDomainB, EmbeddedInstanceQueueService, EmbeddedInstanceQueue])
 class UpdateEmbeddedInstancesServiceSpec extends Specification {
 
-    void "test initializeEmbeddedDomainsMap for creating correct maps for TestDomainA and TestDomainB"() {
-        when: "The getEmbeddedDomainsMap method is called"
-        service.getEmbeddedDomainsMap()
+    void 'test initializeEmbeddedDomainsMap for creating correct maps for TestDomainA and TestDomainB'() {
+        when: 'The getEmbeddedDomainsMap method is called'
+        service.embeddedDomainsMap
 
-        then: "The domainsThatEmbed and embeddedClassFields map should be correctly initialized"
+        then: 'The domainsThatEmbed and embeddedClassFields map should be correctly initialized'
         UpdateEmbeddedInstancesService.domainsThatEmbed == [TestDomainA: [TestDomainB: [[fieldName: 'testDomainA',
                 isFieldArray: false], [fieldName: 'testDomainASet', isFieldArray: true]]]]
         UpdateEmbeddedInstancesService.embeddedClassFields == [TestDomainA: [fieldList: ['testField1', 'status']]]
     }
 
-    void "test resolvePrivateFieldNames for returning field names that needs to be matched for dirty checking"() {
-        when: "The resolveFieldsForDirtiness method is called for EmTestDomainA"
+    void 'test resolvePrivateFieldNames for returning field names that needs to be matched for dirty checking'() {
+        when: 'The resolveFieldsForDirtiness method is called for EmTestDomainA'
         List<String> fieldNames = service.resolveFieldsForDirtiness(EmTestDomainA)
 
-        then: "The field TestFieldA should be returned"
+        then: 'The field TestFieldA should be returned'
         fieldNames == ['testField1', 'status']
     }
 
-    void "test getFieldsToCheckForDirty to get the list of fields to check dirty for a domain"() {
-        given: "Initializing the field list for each domain"
-        service.getEmbeddedDomainsMap()
+    void 'test getFieldsToCheckForDirty to get the list of fields to check dirty for a domain'() {
+        given: 'Initializing the field list for each domain'
+        service.embeddedDomainsMap
 
-        when: "The getFieldsToCheckForDirty method is called for TestDomainA"
-        List<String> fieldNames = service.getFieldsToCheckForDirty(TestDomainA.class.simpleName)
+        when: 'The getFieldsToCheckForDirty method is called for TestDomainA'
+        List<String> fieldNames = service.getFieldsToCheckForDirty(TestDomainA.simpleName)
 
-        then: "The field TestFieldA should be returned"
+        then: 'The field TestFieldA should be returned'
         fieldNames == ['testField1', 'status']
     }
 
-    void "test enqueue for adding EmbeddedInstanceQueue instances for a domain instance"() {
-        given: "An instance of TestDomainA"
+    void 'test enqueue for adding EmbeddedInstanceQueue instances for a domain instance'() {
+        given: 'An instance of TestDomainA'
         TestDomainA testDomainAInstance = new TestDomainA()
-        testDomainAInstance.testField1 = "Test 1"
-        testDomainAInstance.testField2 = "Test 2"
+        testDomainAInstance.testField1 = 'Test 1'
+        testDomainAInstance.testField2 = 'Test 2'
         testDomainAInstance.embeddingNonDomainField = new ObjectId()
         testDomainAInstance.save(flush: true)
 
         assert EmbeddedInstanceQueue.count() == 0
 
-        when: "The enqueue method is called for this domain instance"
+        when: 'The enqueue method is called for this domain instance'
         service.enqueue(testDomainAInstance)
 
-        then: "Instances of EmbeddedInstanceQueue should be created"
-        List<EmbeddedInstanceQueue> embeddedInstanceQueueList = EmbeddedInstanceQueue.list(sort: 'dateCreated')
+        then: 'Instances of EmbeddedInstanceQueue should be created'
+        List<EmbeddedInstanceQueue> embeddedInstanceQueueList = EmbeddedInstanceQueue.list(sort: 'dateCreated', max: 10)
 
         embeddedInstanceQueueList.size() == 2
 
